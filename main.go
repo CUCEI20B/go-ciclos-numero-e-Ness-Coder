5c213494509b000be48db617d2813066a71f025e
Nestor Villarreal <nestor.haro5644@alumnos.udg.mx>
2020-09-28 10:09:25 -0500

    Prueba calculo numero e

--- main.go
+++ main.go
@@ -3,5 +3,22 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var n float64
+	var j float64
+	var i float64
+	var e float64
+	var temp float64
+	
+
+	e = 2.71828
+	fmt.Scan(&n)
+
+	for j = 1; j <= n; j++ {
+		temp = 1 // temp se usara como el factorial
+		for i = 1; i <= j; i++ {
+			temp = temp * j
+		} 
+		e = e + 1.0 /temp
+	}
+	fmt.Println(e)
 }
